6e7ebe37ed55c8c9c40af4cff7bda1172a12486b
Rattle-Brain <danialgo2001@gmail.com>
2024-06-14 13:00:59 +0200

    Refactored producer

---
+++ src/main/java/org/example/producers/MainProducerVerticle.java
@@ -0,0 +1,12 @@
+package org.example.producers;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+
+public class MainProducerVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        vertx.deployVerticle(new UserInputProducerVerticle());
+    }
+}
