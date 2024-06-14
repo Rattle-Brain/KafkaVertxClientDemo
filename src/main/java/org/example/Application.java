11a70cd4bf6e9d068f1663500711f9aecac165ac
Rattle-Brain <danialgo2001@gmail.com>
2024-06-14 11:50:08 +0200

    Refactored to app

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import io.vertx.core.Vertx;
+import org.example.producers.MainProducerVerticle;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Initializing Vertx");
+        Vertx v = Vertx.vertx();
+        v.deployVerticle(new MainProducerVerticle());
+    }
+}
