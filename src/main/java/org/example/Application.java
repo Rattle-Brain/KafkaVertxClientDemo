5020ff4fdca1030aad9cdcc142dcdf33bfa27a86
Rattle-Brain <danialgo2001@gmail.com>
2024-06-17 11:41:00 +0200

    Test producer only

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import io.vertx.core.Vertx;
+import org.example.consumers.MainConsumerVerticle;
 import org.example.producers.MainProducerVerticle;
 
 public class Application {
@@ -9,5 +10,6 @@ public class Application {
         System.out.println("Initializing Vertx");
         Vertx v = Vertx.vertx();
         v.deployVerticle(new MainProducerVerticle());
+        //v.deployVerticle(new MainConsumerVerticle());
     }
 }
