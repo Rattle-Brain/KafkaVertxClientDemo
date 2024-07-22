59b977b6c0f7960d81af80391ff4e137d8d582ad
Daniel Alvarez <daniel.alvarezg@telecable.com>
2024-07-22 14:07:58 +0200

    Config renaming

--- src/main/java/org/example/producers/configs/ProducerConfigs.java
+++ src/main/java/org/example/producers/configs/ProducerConfigs.java
@@ -3,6 +3,8 @@ package org.example.producers.configs;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
 public class ProducerConfigs {
 
     public static Map<String, String> genericProducerConfig = getGenericProducerConfig();
@@ -10,10 +12,11 @@ public class ProducerConfigs {
     public static Map<String, String> getGenericProducerConfig() {
         Map<String, String> config = new HashMap<>();
 
-        config.put("bootstrap.servers", "localhost:9092");
-        config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        config.put("acks", "1");
+        config.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        config.put(VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        config.put(ACKS_CONFIG, "1");
+        config.put(MAX_BLOCK_MS_CONFIG, "10000");
 
         return config;
     }
